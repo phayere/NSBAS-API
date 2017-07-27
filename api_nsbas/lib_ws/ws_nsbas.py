e85355be1726240978b87f92dd8b740806da624f
Franck <Franck@uga>
2017-07-27 15:22:40 +0200

    adding programme return code to info structure

--- api_nsbas/lib_ws/ws_nsbas.py
+++ api_nsbas/lib_ws/ws_nsbas.py
@@ -3,19 +3,33 @@
 # Inclure eventuellement ici :
 # Pour chaque webservice, une fonction realisant le coeur du Execute
 # Pour chaque webservice, une fonction realisant le coeur du GetResult
+import re
 
 # Observons l'etat de progression d'un job
-def getJobStatus(id_du_job,jeton_du_processus, status):
+def getJobStatus(id_du_job,jeton_du_processus, ws_status):
 # Version a perfectionner (!)
+    #{"errorMessage": "", "returnCode": "256", "oarStatus": "Terminated"}
     # Avancement en pourcentage
     percentDone = 50
     terminated = False
     # Accepted / Terminated
+    status = ws_status
+    retcode = ""
+
+    m = re.search('"returnCode" *: *"(\d+)"', ws_status)
+    if m:
+        retcode = m.groups()[0]
+
+    m = re.search('oarStatus.*:.*"(.*)"', ws_status)
+    if m:
+        status = m.groups()[0]
+ 
     jobStatus = {
-	    "StatusInfo": {
+        "StatusInfo": {
 		"JobID": id_du_job,
         "processToken" : jeton_du_processus,
-		"Status": "" +status+ "",
+		"Status":  status,
+        "Retcode" : retcode,
 		"Progress": percentDone
 		}
 	}
